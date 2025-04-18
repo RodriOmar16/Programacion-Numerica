2ab40352881766cf6cae81612f8b2b1239a09d55
RodriOmar16 <rodrigoomarmiranda1@gmail.com>
2025-04-18 13:44:17 -0300

    18042025_13-44

--- FuncionesEmpiricas/src/main.java
+++ FuncionesEmpiricas/src/main.java
@@ -1,18 +1,19 @@
 public class main {
 
 	public static void main(String[] args) {
-		funcionEmpirica f = new funcionEmpirica(4);
+		funcionEmpirica f = new funcionEmpirica(6);
 		
 		f.cargarListaPtos();
 		if(f.controlarPtos()) {
 			f.mostrarListaPtos();
-			f.linealizacion("Exponencial");
+			f.linealizacion("Potencial");
 			System.out.println("\nLuego de linealizar:");
-			f.puntosSeleccionados(0,4);
-			//f.desvios();
+			//f.puntosSeleccionados(1,3);
+			f.desvios();
 			//f.minimosCuadrados();
-			System.out.println("\nParámetros a: "+f.getA());
-			System.out.println("\nParámetros b: "+f.getB());
+			System.out.println("\nParámetros alfa: "+f.getA()); // Valor linealidazos
+			System.out.println("\nParámetros beta: "+f.getB()); // Valor Linealizados
+			// se requiere convertirlos para encontrar a y b correctos
 		}else System.out.println("fallo");
 	}
 
